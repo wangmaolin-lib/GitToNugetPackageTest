1f03ddec9a912eddeb860215cd4aa75aff403efd
wangmaolin <wangmaolin@pay.media>
2021-04-21 10:36:22 +0800

    111

--- GitToNugetPackageTestgit/Program.cs
+++ GitToNugetPackageTestgit/Program.cs
@@ -6,7 +6,8 @@ namespace GitToNugetPackageTestgit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worsssld!");
+            Console.WriteLine("这是一个新版本1.0.1");
+            Console.WriteLine("Hello Worsld!");
         }
     }
 }
