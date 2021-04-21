90dc54ee47d58d8278961e5678e831c1d1b1430f
wangmaolin <wangmaolin@pay.media>
2021-04-21 17:54:52 +0800

    修改解决方案名字

---
+++ GitToNugetPackageTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitToNugetPackageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
